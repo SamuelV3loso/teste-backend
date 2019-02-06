9c93224cc79b959d4661aa9b8bbb0dfc238b1991
SamuelV3loso <samuelveloso42@gmail.com>
2019-02-06 08:29:17 -0300

    Configurando servidor para responder na porta 3000

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const mysql = require('mysql');
 
+const mysql = require('mysql');
+const express = require('express');
+
+var app = express();
+const bodyparser = require('body-parser');
+app.use(bodyparser.json());
+
 var mysqlConnection = mysql.createConnection({
     host:'localhost',
     user: 'root',
@@ -14,3 +21,5 @@ var mysqlConnection = mysql.createConnection({
     else
     console.log('DB connection failed \n Error:' + JSON.stringify(err,undefined, 2));
   })
+
+app.listen(3000, ()=>console.log('Express server is running at: 3000'));
